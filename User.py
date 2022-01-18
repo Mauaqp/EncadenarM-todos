96fc45f2d02c3e3d8844bdd224866a80b1eccf7c
Mauaqp <peraltilla93@hotmail.com>
2022-01-18 12:27:45 -0500

    Entrega

--- User.py
+++ User.py
@@ -4,15 +4,18 @@ class User:
         self.surname = surname
         self.balance = balance
     
+    #Metodos
     def hacer_retiro(self, amount):
         self.nuevoBalance = self.balance - amount
         self.balance = self.nuevoBalance
         print("El nuevo balance es: " + str(self.balance))
+        return self
 
     def hacer_deposito(self, amount):
         self.nuevoBalance = self.balance + amount
         self.balance = self.nuevoBalance
         print("El nuevo balance es: " + str(self.balance))
+        return self
     
     def transferencia(self,other_user,amount):
         self.nuevoBalance = self.balance - amount
@@ -23,8 +26,10 @@ class User:
         print(self.name + " ha transferido " + str(amount) + " a " + other_user.name)
         print(other_user.name +" - " + str(other_user.balance))
         print(self.name +" - " + str(self.balance))
+        return self
         
 
     def imprimeInfo ( self):
         print("----------")
         print(self.name + " " + self.surname + " " +str(self.balance)+" Soles")
+        return self
--- main.py
+++ main.py
@@ -5,38 +5,21 @@ ernesto = User("Ernesto", "Valencia", 15000)
 pedro = User ("Pedro", "Peraltilla", 300000)
 paula = User("Paula", "Rodriguez", 50)
 
+# Revisión con ENCADENAMIENTO DE MÉTODOS
+paula.hacer_retiro(10).hacer_retiro(10).imprimeInfo()
 
-paula.imprimeInfo()
-paula.hacer_retiro(10)
-paula.imprimeInfo()
+# #Haz que el primer usuario haga 3 depósitos y 1 giro, y luego muestra sus balances
 
-#Haz que el primer usuario haga 3 depósitos y 1 giro, y luego muestra sus balances
-
-alberto.hacer_deposito(200) # Depósito 1
-alberto.imprimeInfo()
-alberto.hacer_deposito(500) #Depósito 2
-alberto.imprimeInfo()
-alberto.hacer_deposito(5) #Depósito 3
-alberto.imprimeInfo()
+alberto.hacer_deposito(200).hacer_deposito(500).hacer_deposito(5).imprimeInfo()
 
 # Haz que el segundo usuario haga 2 depósitos y 2 giros, y luego muestra sus balances
-ernesto.imprimeInfo()
-ernesto.hacer_deposito(150)# Depósito 1
-ernesto.hacer_deposito(745)# Depósito 2
-ernesto.hacer_retiro(1000) # Giro 1
-ernesto.hacer_retiro(1450) # Giro 2
-ernesto.imprimeInfo()
+ernesto.imprimeInfo().hacer_deposito(150).hacer_deposito(745).hacer_retiro(1000).hacer_retiro(1450).imprimeInfo()
 
 #Haz que el tercer usuario haga 1 depósito y 3 giros, y luego muestra sus balances
-pedro.imprimeInfo()
-pedro.hacer_deposito(100) #Depósito 1
-pedro.hacer_retiro(13) # Giro 1
-pedro.hacer_retiro(5) # Giro 2
-pedro.hacer_retiro(200) # Giro 3
-pedro.imprimeInfo()
+pedro.imprimeInfo().hacer_deposito(100).hacer_retiro(13).hacer_retiro(5).hacer_retiro(200).imprimeInfo()
 
-#BONUS: Agrega un método transferir_dinero; 
+# BONUS: Agrega un método transferir_dinero; 
 # haz que el primer usuario transfiera dinero al tercer usuario y luego imprime los balances de ambos usuarios
 
-ernesto.transferencia(paula, 12000)
+ernesto.transferencia(paula, 12000).transferencia(pedro, 1200)
 
